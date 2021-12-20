f1f5ce90395cfb22edeeb504a30b82f7647f4635
Julien Lecointe <j.lecointe@caplaser.fr>
2021-12-20 13:37:40 +0100

    Project init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <img
+    alt="Vue logo"
+    src="./assets/logo.png"
+  />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<script setup>
+import HelloWorld from './views/HelloWorld.vue'
+</script>
+
+
+
+<style lang="scss">
+@import "./styles/main.scss";
+</style>
